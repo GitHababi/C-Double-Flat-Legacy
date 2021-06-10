090693859ae9def533ae736d791502a9e3ee8be0
GitHababi <76075036+GitHababi@users.noreply.github.com>
2021-06-10 12:12:55 -0700

    Starting Expression Interpreting.
    yes

---
+++ src/Core/Value.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Double_Flat.Core
+{
+    public class Value
+    {
+        public ValueType DataType;
+        public string Data;
+
+        public Value() { }
+        public Value(string data, ValueType type) { this.Data = data; this.DataType = type; }
+
+        public static Value Default = new Value("0",ValueType.NUMBER);
+    }
+
+    public enum ValueType
+    {
+        STRING,
+        BOOL,
+        NUMBER
+    }
+}
